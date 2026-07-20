6dc82fec1652dd2115dde774f992bf430b7c278a
matrausch <mathias.rausch@gmail.com>
2026-07-20 11:58:39 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 
-echo "Hello Azure";
+echo "<h1>Hello Azure</h1><br /><br />";
 
-echo "Wurde auch langsam mal Zeit, dass hier etwas funktioniert.";
+echo "<h2>Wurde auch langsam mal Zeit, dass hier etwas funktioniert</h2>";
