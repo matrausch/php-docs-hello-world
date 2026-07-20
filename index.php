ad92092337d48905bd1a212278afd7585ff14fcc
matrausch <mathias.rausch@gmail.com>
2026-07-20 14:07:55 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,16 @@
 <html>
 
 <head>
-  <title>Hello Hello</title>
+  <title>Hello Hello Hello</title>
   <style type="text/css">
-    h1, h2 { color: rgba(255,255,255,0.5) }
-    body { background-color: black }
+    h1, h2 { color: rgb(255,255,255) }
+    body { background-color: blue }
   </style>
 </head>
 <body>
-<h1>Hello Azure</h1><br /><br />
-
-<h2>Wurde auch langsam mal Zeit, dass hier etwas funktioniert</h2>
-
-<img src="https://i.imgflip.com/5xzryk.jpg" /><br /><br />
-<div style="width:360px;max-width:100%;"><div style="height:0;padding-bottom:62.5%;position:relative;"><iframe width="360" height="225" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameBorder="0" src="https://imgflip.com/embed/51tvbk"></iframe></div><p><a href="https://imgflip.com/gif/51tvbk">via Imgflip</a></p></div>
+  <h1>Hello Azure</h1><br /><br />
+  <img src="https://i.imgflip.com/5xzryk.jpg" /><br /><br />
+  <div style="width:360px;max-width:100%;"><div style="height:0;padding-bottom:62.5%;position:relative;"><iframe width="360" height="225" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameBorder="0" src="https://imgflip.com/embed/51tvbk"></iframe></div><p><a href="https://imgflip.com/gif/51tvbk">via Imgflip</a></p></div>
 </body>
+  
 </html>
