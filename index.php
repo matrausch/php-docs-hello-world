ce715020d9eda2b3581e7500cf8514dd2a76aa41
matrausch <mathias.rausch@gmail.com>
2026-07-20 12:09:54 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,5 +5,5 @@ echo "<h1>Hello Azure</h1><br /><br />";
 echo "<h2>Wurde auch langsam mal Zeit, dass hier etwas funktioniert</h2>";
 
 ?>
-<img src="https://i.imgflip.com/5xzryk.jpg /><br />
+<img src="https://i.imgflip.com/5xzryk.jpg" /><br /><br />
 <div style="width:360px;max-width:100%;"><div style="height:0;padding-bottom:62.5%;position:relative;"><iframe width="360" height="225" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameBorder="0" src="https://imgflip.com/embed/51tvbk"></iframe></div><p><a href="https://imgflip.com/gif/51tvbk">via Imgflip</a></p></div>
